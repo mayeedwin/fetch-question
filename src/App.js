d1f1baf8d5d89d70b5e4bb9a75a47d2104b00b42
Maye Edwin <mayedwine1@gmail.com>
2019-11-21 16:10:50 +0300

    add hmtl

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ getStudents().then(data => {
     // loop through each student in students
     for (let student of students) {
         if (student.gender == 'Male') {
-            // 1.0 push to male array id of student
+            // 1.0 push to male array 'id' of student
             male.push(student.id);
         } else {
-            // 2.0 push to fmale array id of student
+            // 2.0 push to fmale array 'id' of student
             female.push(student.id);
         }
     }
@@ -23,9 +23,11 @@ getStudents().then(data => {
 
     // print male array length = number of males
     console.log(`Number of Males : ${male.length}`);
-    
     // print female array length = number of females
     console.log(`Number of Females : ${female.length}`);
+    // get element and render data
+       document.querySelector('.male').innerText = `Number of Males : ${male.length}`;
+       document.querySelector('.female').innerText = `Number of Females : ${female.length}`;
 
 }).catch(err => {
     // do something for an error here
